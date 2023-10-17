979cf672490794a26b517fda7a9c341b1203e96d
bolu <morolaribolu@gmail.com>
2023-10-17 22:04:14 +0100

    Add to Cart Functionality

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,32 @@
 var webstore = new Vue({
   el: "#app",
-  data: {},
+  data: {
+    siteheader: "After School Club",
+    product: {
+      id: 1001,
+      subject: "Math",
+      location: "London",
+      price: 1000,
+      image: "ty.jpeg",
+      availableInventory: 5,
+    },
+    showProduct: true,
+    cart: [],
+  },
+  methods: {
+    addToCart: function () {
+      this.cart.push(this.product.id);
+    },
+    showCheckout() {
+      this.showProduct = this.showProduct ? false : true;
+    },
+  },
+  computed: {
+    cartItemCount: function () {
+      return this.cart.length || "";
+    },
+    canAddToCart: function () {
+      return this.product.availableInventory > this.cartItemCount;
+    },
+  },
 });
