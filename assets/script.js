77d24e0fb0b6a3d5a1fb34b73f168cffa8eddb0a
bolu <morolaribolu@gmail.com>
2023-11-06 00:56:53 +0000

    Refactoring with Conditionals and Loops

--- assets/script.js
+++ assets/script.js
@@ -2,30 +2,17 @@ var webstore = new Vue({
   el: "#app",
   data: {
     siteheader: "After School Club",
-    product: {
-      id: 1001,
-      subject: "Math",
-      location: "London",
-      price: 1000,
-      image: "ty.jpeg",
-      availableInventory: 5,
-    },
+    products: products,
     showProduct: true,
     order: {
       firstName: "",
       lastName: "",
-      address: "",
-      city: "",
-      zip: "",
-      state: "",
-      method: "Home",
-      gift: false,
     },
     cart: [],
   },
   methods: {
-    addToCart: function () {
-      this.cart.push(this.product.id);
+    addToCart(product) {
+      this.cart.push(product.id);
     },
     showCheckout() {
       this.showProduct = this.showProduct ? false : true;
@@ -33,13 +20,27 @@ var webstore = new Vue({
     submitForm() {
       alert("Order Submitted!");
     },
+
+    canAddToCart(product) {
+      return product.availableInventory > this.cartCount(product.id);
+    },
+
+    cartCount(id) {
+      let count = 0;
+      for (let i = 0; i < this.cart.length; i++) {
+        if (this.cart[i] === id) {
+          count++;
+        }
+      }
+      return count;
+    },
   },
   computed: {
     cartItemCount: function () {
       return this.cart.length || "";
     },
-    canAddToCart: function () {
-      return this.product.availableInventory > this.cartItemCount;
+    itemsLeft() {
+      return this.product.availableInventory - this.cartItemCount;
     },
   },
 });
